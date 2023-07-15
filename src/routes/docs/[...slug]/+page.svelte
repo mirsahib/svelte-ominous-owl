dac1a25e02c0e329825ce284096cf4a5d38722ba
Mir Sahib <mirsahib24@gmail.com>
2023-07-15 11:16:40 +0600

    :hammer: Add catch all route

---
+++ src/routes/docs/[...slug]/+page.svelte
@@ -0,0 +1,15 @@
+<script>
+    import {page} from "$app/stores"
+    console.log("🚀 ~ file: +page.svelte:3 ~ page:", $page.params)
+
+    const slugArr = $page.params.slug.split('/')
+    console.log("🚀 ~ file: +page.svelte:6 ~ slugArr:", slugArr)
+</script>
+
+<h1>Docs home page</h1>
+
+{#if slugArr.length == 1 }
+<h1>Viewing docs for feature {slugArr[0]}</h1>
+{:else if slugArr.length==2}
+<h1>Viewing docs for feature {slugArr[0]} and concept {slugArr[1]}</h1>
+{/if}
