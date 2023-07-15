16c94b643a4024993d262bb94fbe7c14b6eff0b2
Mir Sahib <mirsahib24@gmail.com>
2023-07-15 11:15:57 +0600

    :hammer: Add dynamic routing

---
+++ src/routes/products/+page.svelte
@@ -0,0 +1,1 @@
+<h1>Product home page</h1>
---
+++ src/routes/products/[productId]/+page.svelte
@@ -0,0 +1,6 @@
+<script>
+    import {page} from "$app/stores"
+    const productId = $page.params.productId
+</script>
+
+<h1>Details of product {productId}</h1>
